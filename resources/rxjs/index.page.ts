0b8bfda4f195e44c2ef724a834d84f8bef97520a
solome <iliyang.cn@gmail.com>
2019-05-28 20:04:27 +0800

    sync code

---
+++ resources/rxjs/index.page.ts
@@ -0,0 +1,7 @@
+import * as Rx from 'rxjs'
+import 'rxjs/add/observable/of'
+
+const __global__: any = window
+__global__.Rx = Rx
+
+console.log(Rx)
--- resources/threejs/l3/examples/index.ts
+++ resources/threejs/l3/examples/index.ts
@@ -1,12 +1,9 @@
 import * as THREE from 'three'
-import { orbitControls } from '@three/controls/OrbitControls'
-import * as dat from 'dat.gui'
 import __global__ from '@three/libs/__global__'
 
 import PlaneCanvas from '@three/l3/utils/PlaneCanvas'
 
 const run = (canvas) => {
-
   const planeCanvas = new PlaneCanvas({ canvas, needAxes: true })
 
   const scene = planeCanvas.scene
@@ -14,7 +11,10 @@ const run = (canvas) => {
   // Cube/Box
   const boxGeometry = new THREE.BoxGeometry(4, 4, 4)
   // const boxMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true})
-  const boxMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000, wireframe: true })
+  const boxMaterial = new THREE.MeshLambertMaterial({
+    color: 0xff0000,
+    wireframe: true,
+  })
   const box = new THREE.Mesh(boxGeometry, boxMaterial)
   box.position.x = -4
   box.position.y = 3
@@ -25,7 +25,10 @@ const run = (canvas) => {
   // Sphere
   const sphereGeometry = new THREE.SphereGeometry(4, 20, 20)
   // const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true})
-  const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff, wireframe: true })
+  const sphereMaterial = new THREE.MeshLambertMaterial({
+    color: 0x7777ff,
+    wireframe: true,
+  })
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
   sphere.position.x = 20
   sphere.position.y = 4
@@ -45,27 +48,22 @@ const run = (canvas) => {
   const controls: Controls = { rotationSpeed: 0.02, bouncingSpeed: 0.04 }
 
   planeCanvas.buildDatGUI((datGUI) => {
-
     datGUI.add(controls, 'rotationSpeed', 0, 0.5)
     datGUI.add(controls, 'bouncingSpeed', 0, 0.5)
-
   })
 
   let bouncingSpeed = 0
   planeCanvas.requestAnimationFrame(() => {
-
     box.rotation.x += controls.rotationSpeed
     box.rotation.y += controls.rotationSpeed
     box.rotation.z += controls.rotationSpeed
 
     bouncingSpeed += controls.bouncingSpeed
-    sphere.position.x = 20 + (10 * (Math.cos(bouncingSpeed)))
-    sphere.position.y = 2 + (10 * Math.abs(Math.sin(bouncingSpeed)))
-
+    sphere.position.x = 20 + 10 * Math.cos(bouncingSpeed)
+    sphere.position.y = 2 + 10 * Math.abs(Math.sin(bouncingSpeed))
   })
 
   return () => planeCanvas.dispose()
-
 }
 
 export default run
